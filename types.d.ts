ec779310c356a4ab7da40a371a70655c113920da
Notsickfox <88145761+scaranaraa@users.noreply.github.com>
2024-05-15 19:43:00 +0530

    fix types

--- types.d.ts
+++ types.d.ts
@@ -1,4 +1,11 @@
-export declare function generate(words: string[] | Map<string, number>, normalize?: boolean, savePath?: string, opts?: {
+/**
+ * 
+ * @param words String, Array of strings or Map of strings and frequencies
+ * @param normalize Normalize frequencies (recommended)
+ * @param savePath Path to save image
+ * @param opts Other options
+ */
+export declare function generate(words: string | string[] | Map<string, number>, normalize?: boolean, savePath?: string, opts?: {
     minSize: number;
     maxSize: number;
     backgroundColor: string;
